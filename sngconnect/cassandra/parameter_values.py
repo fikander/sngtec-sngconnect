ccadf22cc5dc65963db09c019c77f42d537388ab
Mikołaj Siedlarek <m.siedlarek@fatico.com>
2012-09-23 16:11:56 +0200

    sdf

--- sngconnect/cassandra/parameter_values.py
+++ sngconnect/cassandra/parameter_values.py
@@ -17,21 +17,14 @@ class ParameterValues(ColumnFamilyProxy):
 
     @classmethod
     def create(cls, system_manager, keyspace, **additional_kwargs):
-        additional_kwargs.setdefault(
-            'comparator_type',
-            pycassa_types.IntegerType()
-        )
-        additional_kwargs.setdefault(
-            'default_validation_class',
-            pycassa_types.DecimalType()
-        )
-        additional_kwargs.setdefault(
-            'key_validation_class',
-            pycassa_types.CompositeType(
+        additional_kwargs.update({
+            'comparator_type': pycassa_types.IntegerType(),
+            'default_validation_class': pycassa_types.DecimalType(),
+            'key_validation_class': pycassa_types.CompositeType(
                 pycassa_types.IntegerType(),
                 pycassa_types.DateType()
-            )
-        )
+            ),
+        })
         super(ParameterValues, cls).create(
             system_manager,
             keyspace,
@@ -42,7 +35,7 @@ class ParameterValues(ColumnFamilyProxy):
         rows = {}
         key_index = ParameterValuesKeyIndex()
         for measurement_datetime, value in data_points:
-            key = self._row_key(parameter_id, measurement_datetime.date())
+            key = self._row_key(parameter_id, measurement_datetime)
             rows.setdefault(key, {})
             rows[key][measurement_datetime] = value
         self.column_family.batch_insert(rows)
@@ -52,23 +45,28 @@ class ParameterValues(ColumnFamilyProxy):
         )
 
     def get_data_points(self, parameter_id, start_date=None, end_date=None):
-        if start_date is None:
-            start_date = datetime.datetime.min
-        if end_date is None:
-            end_date = datetime.datetime.max
-        # Setting column count to Cassandra's maximum. We assume that
-        # clients of this API know what they're doing.
-        column_count = 2000000000
-        if start_date.date() == end_date.date():
+        kwargs = {
+            # Setting column count to Cassandra's maximum. We assume that
+            # clients of this API know what they're doing.
+            'column_count': 2000000000,
+        }
+        if start_date is not None:
+            kwargs['column_start'] = start_date
+            start_day = start_date.date()
+        else:
+            start_day = None
+        if end_date is not None:
+            kwargs['column_finish'] = end_date
+            end_day = end_date.date()
+        else:
+            end_day = None
+        if start_day is not None and start_day == end_day:
             # Data points from single day requested, we don't need to use
             # multiget.
-            result = self.column_family.get(
-                self._row_key(parameter_id, start_date.date()),
-                column_start=start_date,
-                column_finish=end_date,
-                column_count=column_count
-            )
-            return result.items()
+            return self.column_family.get(
+                self._row_key(parameter_id, start_date),
+                **kwargs
+            ).items()
         else:
             # Data points from possibly many days request, we have to use
             # multiget.
@@ -81,9 +79,7 @@ class ParameterValues(ColumnFamilyProxy):
             keys = [self._row_key(parameter_id, date) for date in dates]
             result = self.column_family.multiget(
                 keys,
-                column_start=start_date,
-                column_finish=end_date,
-                column_count=column_count
+                **kwargs
             )
             data_points = []
             for key, columns in result.iteritems():
@@ -94,7 +90,34 @@ class ParameterValues(ColumnFamilyProxy):
     def _row_key(cls, parameter_id, date):
         if isinstance(date, datetime.date):
             date = datetime.datetime.combine(date, datetime.time.min)
+        elif isinstance(date, datetime.datetime):
+            date = date.replace(
+                hour=0,
+                minute=0,
+                second=0,
+                microsecond=0
+            )
         return (parameter_id, date)
+
+class HourlyAverages(ColumnFamilyProxy):
+
+    _column_family_name = 'HourlyAverages'
+
+    @classmethod
+    def create(cls, system_manager, keyspace, **additional_kwargs):
+        additional_kwargs.update({
+            'comparator_type': pycassa_types.DateType(),
+            'default_validation_class': pycassa_types.DecimalType(),
+            'key_validation_class': pycassa_types.CompositeType(
+                pycassa_types.IntegerType(),
+                pycassa_types.DateType()
+            ),
+        })
+        super(HourlyAverages, cls).create(
+            system_manager,
+            keyspace,
+            **additional_kwargs
+        )
 
 class ParameterValuesKeyIndex(ColumnFamilyProxy):
 
@@ -102,27 +125,16 @@ class ParameterValuesKeyIndex(ColumnFamilyProxy):
 
     @classmethod
     def create(cls, system_manager, keyspace, **additional_kwargs):
-        additional_kwargs.setdefault(
-            'comparator_type',
-            pycassa_types.IntegerType()
-        )
-        additional_kwargs.setdefault(
-            'default_validation_class',
-            pycassa_types.BytesType()
-        )
-        additional_kwargs.setdefault(
-            'key_validation_class',
-            pycassa_types.IntegerType()
-        )
+        additional_kwargs.update({
+            'comparator_type': pycassa_types.DateType(),
+            'default_validation_class': pycassa_types.BytesType(),
+            'key_validation_class': pycassa_types.IntegerType(),
+        })
         super(ParameterValuesKeyIndex, cls).create(
             system_manager,
             keyspace,
             **additional_kwargs
         )
-
-    def __init__(self):
-        super(ParameterValuesKeyIndex, self).__init__()
-        self.column_family.column_name_class = MicrosecondTimestampType()
 
     def add_dates(self, parameter_id, dates):
         values = dict((
@@ -133,16 +145,15 @@ class ParameterValuesKeyIndex(ColumnFamilyProxy):
         self.column_family.insert(parameter_id, values)
 
     def get_dates(self, parameter_id, start_date=None, end_date=None):
-        if start_date is None:
-            start_date = datetime.datetime.min
-        if end_date is None:
-            end_date = datetime.datetime.max
+        kwargs = {}
+        if start_date is not None:
+            kwargs['column_start'] = start_date
+        if end_date is not None:
+            kwargs['column_finish'] = end_date
         try:
-            dates = self.column_family.get(
-                parameter_id,
-                column_start=start_date,
-                column_finish=end_date
-            ).keys()
+            dates = self.column_family.get(parameter_id, **kwargs).keys()
             return [date.date() for date in dates]
         except pycassa.NotFoundException:
             return []
+        except:
+            print "START: %s\nEND: %s" % (start_date, end_date)
