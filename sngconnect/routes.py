3ab4c4461d8821ec72cf6a45859e3b57285c3e79
Mikołaj Siedlarek <m.siedlarek@fatico.com>
2012-09-28 12:32:42 +0200

    deleted development views

--- sngconnect/routes.py
+++ sngconnect/routes.py
@@ -1,8 +1,5 @@
 ROUTES = (
-    ('dev.index', ''),
-    ('dev.system', 's/{system_id:\d+}'),
-    ('dev.parameter', 'p/{parameter_id:\d+}'),
-
+    # sngconnect.api
     (
         'sngconnect.api.system_parameter',
         'api/v1/feeds/{system_id:\d+}/datastreams/{parameter_id:\d+}.json'
