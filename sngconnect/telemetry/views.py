ef8316b2e89660d4d6a66d6f5efa4cc54efcb43e
Mikołaj Siedlarek <m.siedlarek@fatico.com>
2012-09-30 10:56:07 +0200

    view

--- sngconnect/telemetry/views.py
+++ sngconnect/telemetry/views.py
@@ -1,4 +1,11 @@
+# -*- coding: utf-8 -*-
+
+import datetime
+
+import pytz
+from sqlalchemy.orm import exc as database_exceptions
 from pyramid.view import view_config
+from pyramid import httpexceptions
 
 from sngconnect.database import DBSession, System
 
@@ -26,4 +33,46 @@ def dashboard(request):
             }
             for system in systems
         ],
+    }
+
+@view_config(
+    route_name='sngconnect.telemetry.system_dashboard',
+    request_method='GET',
+    renderer='sngconnect.telemetry:templates/system_dashboard.jinja2'
+)
+def system_dashboard(request):
+    try:
+        system = DBSession.query(System).filter(
+            System.id == request.matchdict['system_id']
+        ).one()
+    except database_exceptions.NoResultFound:
+        raise httpexceptions.HTTPNotFound()
+    return {
+        # FIXME
+        'active_alarms': [
+            {
+                'activation_date': pytz.timezone('Europe/Warsaw').localize(
+                    datetime.datetime(2012, 12, 1, 15, 16, 45)
+                ),
+                'parameter': u"nazwa parametru",
+                'type': u"temperatura kotła",
+                'description': u"jakiś niewielki błąd",
+            },
+            {
+                'activation_date': pytz.timezone('Europe/Warsaw').localize(
+                    datetime.datetime(2012, 12, 1, 9, 21, 4)
+                ),
+                'parameter': u"temperatura wyjścia B",
+                'type': u"wartość minimalna przekroczona",
+                'description': u"jakiś straszliwy błąd",
+            },
+        ],
+        'system': {
+            'id': system.id,
+            'name': system.name,
+            'dashboard_url': request.route_url(
+                'sngconnect.telemetry.system_dashboard',
+                system_id=system.id
+            ),
+        }
     }
