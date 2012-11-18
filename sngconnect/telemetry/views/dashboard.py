a98a3bcbd9053daad2ffccb0564c7912962e9d60
Mikołaj Siedlarek <m.siedlarek@fatico.com>
2012-11-18 08:55:32 +0100

    dashboard improvement

--- sngconnect/telemetry/views/dashboard.py
+++ sngconnect/telemetry/views/dashboard.py
@@ -4,6 +4,7 @@ from pyramid.view import view_config
 from pyramid.security import authenticated_userid
 
 from sngconnect.database import DBSession, Feed
+from sngconnect.cassandra import alarms as alarms_store
 
 @view_config(
     route_name='sngconnect.telemetry.dashboard',
@@ -16,23 +17,28 @@ def dashboard(request):
     feeds = DBSession.query(Feed).filter(
         Feed.feed_users.any(user_id=user_id)
     ).order_by(Feed.name)
+    serialized_feeds = []
+    feeds_with_alarm_active = 0
+    for feed in feeds:
+        active_alarms = alarms_store.Alarms().get_active_alarms(feed.id)
+        active_alarm_count = sum(
+            map(len, active_alarms.itervalues())
+        )
+        if active_alarm_count > 0:
+            feeds_with_alarm_active += 1
+        serialized_feeds.append({
+            'id': feed.id,
+            'name': feed.name,
+            'latitude': feed.latitude,
+            'longitude': feed.longitude,
+            'template_name': feed.template_name,
+            'dashboard_url': request.route_url(
+                'sngconnect.telemetry.feed_dashboard',
+                feed_id=feed.id
+            ),
+            'active_alarm_count': active_alarm_count,
+        })
     return {
-        'feeds_with_alarm_active': 2, # FIXME
-        'feeds': [
-            {
-                'id': feed.id,
-                'name': feed.name,
-                'latitude': feed.latitude,
-                'longitude': feed.longitude,
-                'dashboard_url': request.route_url(
-                    'sngconnect.telemetry.feed_dashboard',
-                    feed_id=feed.id
-                ),
-                # FIXME
-                'alarm_count': 5,
-                'feed': u"water pump",
-                'owner': u"Some owner",
-            }
-            for feed in feeds
-        ],
+        'feeds_with_alarm_active': feeds_with_alarm_active,
+        'feeds': serialized_feeds,
     }
