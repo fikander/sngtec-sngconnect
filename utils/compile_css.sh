8c5b2913fcdabe0cdbb61bfdc8a172617f167213
Tomasz Kustrzynski <tomasz.kustrzynski@gmail.com>
2013-09-07 13:46:28 +0100

    upgraded wuxia to v1.6

--- utils/compile_css.sh
+++ utils/compile_css.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+#
+# To install lessc:
+#
+# OSX:
+#  - install Homebrew ('ruby -e "$(curl -fsSL https://raw.github.com/mxcl/homebrew/go)"')
+#  - install node.js ('brew install node')
+#  - install npm ('curl https://npmjs.org/install.sh | sh')
+#  - install less ('npm install -g less')
+#
 
 pushd sngconnect/static/wuxia
 lessc less/style.less > css/sngconnect.css
